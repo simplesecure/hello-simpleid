8301ded97c63e9ddd9915b416827ac5d507040ec
prabhaav <prabhaav.bhardwaj@gmail.com>
2019-08-26 12:22:53 -0700

    updated sdk

--- src/Banner.js
+++ src/Banner.js
@@ -4,7 +4,7 @@ export default class Banner extends React.Component {
   render() {
     return (
       <div id="banner">
-        <h3><strong>Want to add SimpleID to your app? Sign up for a free account <a href="https://app.simpleid.xyz/?t=demo_app" target="_blank" rel="noreferrer noopener">here</a></strong>.</h3>
+        <h2><strong>Want to add SimpleID to your app? Sign up for a free account <a href="https://app.simpleid.xyz/?t=demo_app" target="_blank" rel="noreferrer noopener">here</a></strong>.</h2>
       </div>
     )
   }
